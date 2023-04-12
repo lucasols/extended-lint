3e351f5be933bb394bb2db2d90842e0fa3fc531f
lucaslos <lucas@sugestly.com>
2023-04-12 13:50:50 -0300

    Check arrow functions too

--- src/rules/no-unused-object-type-properties.ts
+++ src/rules/no-unused-object-type-properties.ts
@@ -1,4 +1,5 @@
 import { AST_NODE_TYPES, ESLintUtils, TSESTree } from '@typescript-eslint/utils'
+import { RuleContext } from '@typescript-eslint/utils/dist/ts-eslint'
 
 const createRule = ESLintUtils.RuleCreator(
   (name) => `https://example.com/rule/${name}`,
@@ -44,81 +45,88 @@ const rule = createRule({
   },
   defaultOptions: [],
   create: function (context) {
-    return {
-      FunctionDeclaration: function (node) {
-        for (const param of node.params) {
-          if (param.type === 'ObjectPattern' && param.typeAnnotation) {
-            const declaredProperties = new Map<string, TSESTree.Node>()
+    function checkParams(params: TSESTree.Parameter[]) {
+      for (const param of params) {
+        if (param.type === 'ObjectPattern' && param.typeAnnotation) {
+          const declaredProperties = new Map<string, TSESTree.Node>()
+
+          if (
+            param.typeAnnotation.typeAnnotation.type ===
+            AST_NODE_TYPES.TSTypeLiteral
+          ) {
+            extendMap(
+              declaredProperties,
+              ...getTypeLiteralMembers(param.typeAnnotation.typeAnnotation),
+            )
+          } else {
+            const typeAnnotation = param.typeAnnotation.typeAnnotation
 
             if (
-              param.typeAnnotation.typeAnnotation.type ===
-              AST_NODE_TYPES.TSTypeLiteral
+              typeAnnotation.type === AST_NODE_TYPES.TSTypeReference &&
+              typeAnnotation.typeName.type === AST_NODE_TYPES.Identifier
             ) {
-              extendMap(
-                declaredProperties,
-                ...getTypeLiteralMembers(param.typeAnnotation.typeAnnotation),
-              )
-            } else {
-              const typeAnnotation = param.typeAnnotation.typeAnnotation
+              const typeName = typeAnnotation.typeName.name
+
+              const resolved = context
+                .getScope()
+                .references.find(
+                  (reference) => reference.identifier.name === typeName,
+                )?.resolved
+
+              if (!resolved || resolved.references.length > 1) {
+                continue
+              }
+
+              const type = resolved?.defs[0]?.node
 
               if (
-                typeAnnotation.type === AST_NODE_TYPES.TSTypeReference &&
-                typeAnnotation.typeName.type === AST_NODE_TYPES.Identifier
+                type?.type === AST_NODE_TYPES.TSTypeAliasDeclaration &&
+                type.typeAnnotation.type === AST_NODE_TYPES.TSTypeLiteral
               ) {
-                const typeName = typeAnnotation.typeName.name
-
-                const resolved = context
-                  .getScope()
-                  .references.find(
-                    (reference) => reference.identifier.name === typeName,
-                  )?.resolved
-
-                if (!resolved || resolved.references.length > 1) {
-                  continue
-                }
-
-                const type = resolved?.defs[0]?.node
-
-                if (
-                  type?.type === AST_NODE_TYPES.TSTypeAliasDeclaration &&
-                  type.typeAnnotation.type === AST_NODE_TYPES.TSTypeLiteral
-                ) {
-                  extendMap(
-                    declaredProperties,
-                    ...getTypeLiteralMembers(type.typeAnnotation),
-                  )
-                }
-              }
-            }
-
-            if (declaredProperties.size === 0) {
-              continue
-            }
-
-            const destructuredProperties: string[] = []
-
-            for (const property of param.properties) {
-              if (
-                property.type === AST_NODE_TYPES.Property &&
-                property.key.type === AST_NODE_TYPES.Identifier
-              ) {
-                destructuredProperties.push(property.key.name)
-              }
-            }
-
-            for (const [declaredProperty, node] of declaredProperties) {
-              if (!destructuredProperties.includes(declaredProperty)) {
-                context.report({
-                  node: node,
-                  messageId: 'unusedObjectTypeProperty',
-                  data: {
-                    propertyName: declaredProperty,
-                  },
-                })
+                extendMap(
+                  declaredProperties,
+                  ...getTypeLiteralMembers(type.typeAnnotation),
+                )
               }
             }
           }
+
+          if (declaredProperties.size === 0) {
+            continue
+          }
+
+          const destructuredProperties: string[] = []
+
+          for (const property of param.properties) {
+            if (
+              property.type === AST_NODE_TYPES.Property &&
+              property.key.type === AST_NODE_TYPES.Identifier
+            ) {
+              destructuredProperties.push(property.key.name)
+            }
+          }
+
+          for (const [declaredProperty, node] of declaredProperties) {
+            if (!destructuredProperties.includes(declaredProperty)) {
+              context.report({
+                node: node,
+                messageId: 'unusedObjectTypeProperty',
+                data: {
+                  propertyName: declaredProperty,
+                },
+              })
+            }
+          }
         }
+      }
+    }
+
+    return {
+      FunctionDeclaration: function (node) {
+        checkParams(node.params)
+      },
+      ArrowFunctionExpression: function (node) {
+        checkParams(node.params)
       },
     }
   },
