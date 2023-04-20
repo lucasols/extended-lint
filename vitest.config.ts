54b3165c3ca75fc0ed3680e260f231e21c5f8f11
lucaslos <lucas@sugestly.com>
2023-04-20 10:40:55 -0300

    fix allowOnly

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 
+const isDev = process.env.NODE_ENV === 'development'
+
 export default defineConfig({
   test: {
     include: ['tests/*.test.{ts,tsx}'],
     testTimeout: 2_000,
+    allowOnly: isDev,
   },
 })
