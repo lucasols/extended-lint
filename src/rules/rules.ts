3b764ec0b3e2c98d29ff34d708486ecc5a811d01
lucaslos <lucas@sugestly.com>
2023-09-26 15:23:18 -0300

    add exhaustive-deps

--- src/rules/rules.ts
+++ src/rules/rules.ts
@@ -2,10 +2,12 @@ import { noCallWithInferedGenerics } from './no-call-with-infered-generics'
 import { noCommentedOutCode } from './no-commented-code'
 import { noUnusedObjectTypeProperties } from './no-unused-type-props-in-args'
 import ReactHooksESLintRule from './rules-of-hooks'
+import ExhaustiveDepsESLintRule from './exhaustive-deps'
 
 export const rules = {
   [noUnusedObjectTypeProperties.name]: noUnusedObjectTypeProperties.rule,
   [noCommentedOutCode.name]: noCommentedOutCode.rule,
   [noCallWithInferedGenerics.name]: noCallWithInferedGenerics.rule,
   ['rules-of-hooks']: ReactHooksESLintRule,
+  ['exhaustive-deps']: ExhaustiveDepsESLintRule,
 }
