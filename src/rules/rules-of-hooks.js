b616f829d23c61024b9cfc1c3c5ac405bda02067
lucaslos <lucas@sugestly.com>
2024-11-24 21:51:56 -0300

    refact: remove unsupported "using" declaration reporting from rules-of-hooks for cleaner codebase

--- src/rules/rules-of-hooks.js
+++ src/rules/rules-of-hooks.js
@@ -162,15 +162,6 @@ export const rulesOfHooksESLintRule = {
                 })
               }
             },
-
-            VariableDeclaration(node) {
-              if (node.kind === 'using') {
-                context.report({
-                  node,
-                  message: `"using" syntax is not yet supported by React Compiler.`,
-                })
-              }
-            },
           }
         }
       }
