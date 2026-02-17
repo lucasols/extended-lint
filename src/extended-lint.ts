f6d01f5e679c3cc5e1dc72ffcac1ffb1b34df55b
lucasols <email@lucassantos.net>
2026-02-17 00:04:17 -0300

    feat: export extendedLintPlugin as default for easier usage

--- src/extended-lint.ts
+++ src/extended-lint.ts
@@ -4,3 +4,6 @@ import { rules } from './rules/rules'
 export const extendedLintPlugin: TSESLint.FlatConfig.Plugin = {
   rules,
 }
+
+// eslint-disable-next-line @ls-stack/no-default-export -- needed for oxlint
+export default extendedLintPlugin
